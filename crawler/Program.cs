fe783b854d52f0de1b3ff289cf71a76ab0e73bdc
Bastian Asmussen <bastian@asmussen.tech>
2025-02-26 14:12:32 +0100

    feat: enhance sitemap crawler to handle multiple URLs

--- crawler/Program.cs
+++ crawler/Program.cs
@@ -1,33 +1,41 @@
-﻿using System;
-using System.IO;
-using System.Net.Http;
-using System.Threading.Tasks;
-using System.Xml.Linq;
-using System.Linq;
+﻿using System.Xml;
 
 class Program
 {
     static async Task Main(string[] args)
     {
-        string sitemapUrls = "https://www.fcomputer.dk/sitemap/dk/sitemap.products1.xml";
-        string outputFilePath = "sitemap_items.txt";
+        var sitemapUrls = new List<string>([
+                "https://www.fcomputer.dk/sitemap/dk/sitemap.products1.xml",
+                "https://www.fcomputer.dk/sitemap/dk/sitemap.products2.xml",
+                "https://www.fcomputer.dk/sitemap/dk/sitemap.products3.xml",
+                "https://www.fcomputer.dk/sitemap/dk/sitemap.products4.xml",
+        ]);
 
         try
         {
             HttpClient client = new HttpClient();
-            string xmlContent = await client.GetStringAsync(sitemapUrls);
-
-            var urls = XDocument.Parse(xmlContent).Descendants("urlset");
-            foreach (var url in urls)
+            foreach (var sitemapUrl in sitemapUrls)
             {
-                Console.WriteLine($"URL: {url.Value}");
-            }
+                string xmlContent = await client.GetStringAsync(sitemapUrl);
 
-            Console.WriteLine("All items have been written to " + outputFilePath);
+                var xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml(xmlContent);
+
+                var urls = new List<string>();
+                foreach (XmlNode node in xmlDoc.GetElementsByTagName("url"))
+                {
+                    var loc = node["loc"];
+                    if (loc == null) continue;
+
+                    urls.Add(loc.InnerText);
+                }
+
+                Console.WriteLine($"Found {urls.Count} product URLs in {sitemapUrl}.");
+            }
         }
-        catch (Exception ex)
+        catch (Exception e)
         {
-            Console.WriteLine("An error occurred: " + ex.Message);
+            Console.WriteLine($"An error occurred: {e.Message}");
         }
     }
 }
